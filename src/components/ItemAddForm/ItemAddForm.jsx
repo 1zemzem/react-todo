3eed8363d13d5c315e9a8fcb1630d1b781a310cf
1zemzem <1zemliakova@gmail.com>
2021-09-06 11:41:21 +0300

    setState

---
+++ src/components/ItemAddForm/ItemAddForm.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './ItemAddForm.css';
+
+export default class ItemAddForm extends Component {
+
+  render() {
+    return (
+      <div className="item-add-form">
+        <button type="button" 
+        className="btn btn-outline-secondary"
+        onClick={() =>this.props.onItemAdded('Hello')}>
+          Add Item
+          </button>
+      </div>
+    )
+  }
+}
